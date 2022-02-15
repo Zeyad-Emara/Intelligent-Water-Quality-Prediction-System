cc16becd442af25f29110d8abe073056fc63fbf5
aiminer357 <aiminer357@gmail.com>
2022-02-15 20:10:50 +0800

    Working Predictiing model

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from template import Ui_MainWindow
 # from PyQt6 import QtCore as qtc
-from PyQt6 import QtWidgets as qtw
+from PyQt6 import QtWidgets as Qtw
 
 # for exe file compilation
 #import sklearn.utils._typedefs
@@ -18,8 +18,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
+from joblib import dump, load
 
-class Window(qtw.QMainWindow):
+
+class Window(Qtw.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(Window, self).__init__(*args, **kwargs)
@@ -39,11 +41,11 @@ class Window(qtw.QMainWindow):
         self.ui.statusbar.showMessage("No dataset found")
 
         # Draw canvas and Toolbar
-        self.layout = qtw.QVBoxLayout()
+        self.layout = Qtw.QVBoxLayout()
         self.static_canvas = FigureCanvasQTAgg(Figure(figsize=(10, 10)))
-        self.layout.addWidget(NavigationToolbar(self.static_canvas, self))
+        #self.layout.addWidget(NavigationToolbar(self.static_canvas, self.centralWidget()))
         self.graph = self.static_canvas.figure.subplots()
-        self.layout.addWidget(self.static_canvas)
+        #self.layout.addWidget(self.static_canvas)
         self.ui.graphWidget.setLayout(self.layout)
 
         # add import button function
@@ -60,8 +62,8 @@ class Window(qtw.QMainWindow):
 
     def find_csv(self):
         try:
-            self.filePath = qtw.QFileDialog.getOpenFileName(filter="csv (*.csv)")[0]
-            self.build_model()
+            self.filePath = Qtw.QFileDialog.getOpenFileName(filter="csv (*.csv)")[0]
+            #self.build_model()
         except:
             self.ui.statusbar.showMessage("No dataset selected")
 
@@ -87,14 +89,17 @@ class Window(qtw.QMainWindow):
 
     def prediction(self):
         try:
+            self.reg = load('resource/WQIModelv1.pkl')
+            self.std = load('resource/StdScaler.pkl')
             value = self.read_table_data()
             value = np.array(value).reshape(1, -1)
             self.predict_input = self.std.transform(value)
             self.predict_value = self.reg.predict(self.predict_input)
             self.ui.resultOutput.clear()
             self.ui.resultOutput.insertPlainText(str(self.predict_value[0]))
-        except:
-            self.ui.statusbar.showMessage("")
+        except Exception as e:
+            print(e)
+            self.ui.statusbar.showMessage("fail")
 
     def read_table_data(self):
         item = []
@@ -105,6 +110,6 @@ class Window(qtw.QMainWindow):
 
 
 if __name__ == "__main__":
-    app = qtw.QApplication(sys.argv)
+    app = Qtw.QApplication(sys.argv)
     window = Window()
     sys.exit(app.exec())
