76d278f2768c81dab83950052c6032c276426573
Jason0w0 <jasonthesaber@gmail.com>
2022-02-23 19:03:43 +0800

    add all view menubar's function

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import sys
 
 from PyQt6.QtWidgets import QLineEdit, QItemDelegate, QWidget, QComboBox, QTableView, QStyledItemDelegate, QDialog
 
+import template
 from template import Ui_MainWindow
 from about_template import Ui_Dialog as AboutDialog
-from PyQt6.QtCore import QRegularExpression, QRect
-from PyQt6 import QtWidgets as Qtw, QtCore, QtGui
+from PyQt6.QtCore import QRegularExpression, QRect, QCoreApplication
+from PyQt6 import QtWidgets as Qtw, QtCore, QtGui, QtWidgets
 from PyQt6.QtGui import QDoubleValidator, QValidator, QRegularExpressionValidator, QPen, QColor
 
 # for exe file compilation
 import sklearn.utils._typedefs
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 from matplotlib.backends.backend_qtagg import (FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar)
@@ -68,8 +70,8 @@ class Window(Qtw.QMainWindow):
         self.ui.resultOutput.setReadOnly(True)
 
         # restrict user's input in the table
-        delegate = TableWidgetDelegate()
-        self.ui.predictionTableWidget.setItemDelegateForColumn(0, delegate)
+        self.delegate = TableWidgetDelegate()
+        self.ui.predictionTableWidget.setItemDelegateForColumn(0, self.delegate)
 
         # add default model
         try:
@@ -81,15 +83,20 @@ class Window(Qtw.QMainWindow):
         # add about page
         self.ui.actionAbout.triggered.connect(self.build_about)
 
+        # add close function
+        self.ui.actionClose.triggered.connect(QCoreApplication.instance().quit)
+
+        # add import docker widget
+        self.ui.actionImport_widget.triggered.connect(self.import_docker)
+
+        # add prediction docker widget
+        self.ui.actionPrediction_widget.triggered.connect(self.prediction_docker)
+
+        # add result docker widget
+        self.ui.actionResult_widget.triggered.connect(self.result_docker)
+
         # Your code ends here
         self.show()
-
-    def build_about(self):
-        dialog = QDialog(self)
-        dialog.ui = AboutDialog()
-        dialog.ui.setupUi(dialog)
-        dialog.exec()
-        dialog.show()
 
     def find_csv(self):
         try:
@@ -192,6 +199,155 @@ class Window(Qtw.QMainWindow):
             self.ui.actionAnalysis_data.setStatusTip("hello")
         except Exception as e:
             print(e)
+
+    def import_docker(self):
+        self.ui.importDockWidget = QtWidgets.QDockWidget(self)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Preferred,
+                                           QtWidgets.QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.ui.importDockWidget.sizePolicy().hasHeightForWidth())
+        self.ui.importDockWidget.setSizePolicy(sizePolicy)
+        self.ui.importDockWidget.setMinimumSize(QtCore.QSize(316, 86))
+        self.ui.importDockWidget.setBaseSize(QtCore.QSize(0, 0))
+        self.ui.importDockWidget.setLayoutDirection(QtCore.Qt.LayoutDirection.LeftToRight)
+        self.ui.importDockWidget.setObjectName("importDockWidget")
+        self.ui.importDockWidgetContents = QtWidgets.QWidget()
+        self.ui.importDockWidgetContents.setObjectName("importDockWidgetContents")
+        self.ui.importSplitter = QtWidgets.QSplitter(self.ui.importDockWidgetContents)
+        self.ui.importSplitter.setGeometry(QtCore.QRect(10, 10, 161, 51))
+        self.ui.importSplitter.setOrientation(QtCore.Qt.Orientation.Vertical)
+        self.ui.importSplitter.setObjectName("importSplitter")
+        self.ui.importLabel = QtWidgets.QLabel(self.ui.importSplitter)
+        self.ui.importLabel.setObjectName("importLabel")
+        self.ui.importPushButton = QtWidgets.QPushButton(self.ui.importSplitter)
+        self.ui.importPushButton.setObjectName("importPushButton")
+        self.ui.importDockWidget.setWidget(self.ui.importDockWidgetContents)
+        self.ui.importLabel.setText("Import File")
+        self.ui.importPushButton.setText("Choose File")
+        self.ui.importPushButton.clicked.connect(self.find_csv)
+        self.addDockWidget(QtCore.Qt.DockWidgetArea(1), self.ui.importDockWidget)
+
+    def prediction_docker(self):
+        self.ui.predictionDockWidget = QtWidgets.QDockWidget(self)
+        self.ui.predictionDockWidget.setMinimumSize(QtCore.QSize(316, 322))
+        self.ui.predictionDockWidget.setObjectName("predictionDockWidget")
+        self.ui.predictionDockWidgetContents = QtWidgets.QWidget()
+        self.ui.predictionDockWidgetContents.setObjectName("predictionDockWidgetContents")
+        self.ui.layoutWidget = QtWidgets.QWidget(self.ui.predictionDockWidgetContents)
+        self.ui.layoutWidget.setGeometry(QtCore.QRect(10, 10, 291, 291))
+        self.ui.layoutWidget.setObjectName("layoutWidget")
+        self.ui.gridLayout = QtWidgets.QGridLayout(self.ui.layoutWidget)
+        self.ui.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.ui.gridLayout.setObjectName("gridLayout")
+        self.ui.predictionLabel = QtWidgets.QLabel(self.ui.layoutWidget)
+        self.ui.predictionLabel.setObjectName("predictionLabel")
+        self.ui.gridLayout.addWidget(self.ui.predictionLabel, 0, 0, 1, 1)
+        self.ui.autofillPushButton = QtWidgets.QPushButton(self.ui.layoutWidget)
+        self.ui.autofillPushButton.setObjectName("autofillPushButton")
+        self.ui.gridLayout.addWidget(self.ui.autofillPushButton, 2, 0, 1, 1)
+        self.ui.predictPushButton = QtWidgets.QPushButton(self.ui.layoutWidget)
+        self.ui.predictPushButton.setObjectName("predictPushButton")
+        self.ui.gridLayout.addWidget(self.ui.predictPushButton, 2, 1, 1, 1)
+        self.ui.predictionTableWidget = QtWidgets.QTableWidget(self.ui.layoutWidget)
+        self.ui.predictionTableWidget.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.DefaultContextMenu)
+        self.ui.predictionTableWidget.setAutoFillBackground(True)
+        self.ui.predictionTableWidget.setInputMethodHints(QtCore.Qt.InputMethodHint.ImhPreferNumbers)
+        self.ui.predictionTableWidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.ui.predictionTableWidget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.ui.predictionTableWidget.setAutoScroll(True)
+        self.ui.predictionTableWidget.setProperty("showDropIndicator", True)
+        self.ui.predictionTableWidget.setDragDropOverwriteMode(True)
+        self.ui.predictionTableWidget.setAlternatingRowColors(True)
+        self.ui.predictionTableWidget.setGridStyle(QtCore.Qt.PenStyle.SolidLine)
+        self.ui.predictionTableWidget.setObjectName("predictionTableWidget")
+        self.ui.predictionTableWidget.setColumnCount(1)
+        self.ui.predictionTableWidget.setRowCount(7)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setVerticalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setVerticalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setVerticalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setVerticalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setVerticalHeaderItem(4, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setVerticalHeaderItem(5, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setVerticalHeaderItem(6, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.ui.predictionTableWidget.setHorizontalHeaderItem(0, item)
+        self.ui.predictionTableWidget.horizontalHeader().setVisible(True)
+        self.ui.predictionTableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.ui.predictionTableWidget.horizontalHeader().setSortIndicatorShown(False)
+        self.ui.predictionTableWidget.horizontalHeader().setStretchLastSection(True)
+        self.ui.predictionTableWidget.verticalHeader().setVisible(True)
+        self.ui.predictionTableWidget.verticalHeader().setCascadingSectionResizes(False)
+        self.ui.predictionTableWidget.verticalHeader().setHighlightSections(True)
+        self.ui.predictionTableWidget.verticalHeader().setSortIndicatorShown(False)
+        self.ui.predictionTableWidget.verticalHeader().setStretchLastSection(True)
+        self.ui.gridLayout.addWidget(self.ui.predictionTableWidget, 1, 0, 1, 2)
+        self.ui.predictionDockWidget.setWidget(self.ui.predictionDockWidgetContents)
+        self.ui.predictPushButton.clicked.connect(self.prediction)
+        self.ui.predictionLabel.setText("Prediction")
+        self.ui.autofillPushButton.setText("Autofill")
+        self.ui.predictPushButton.setText("Predict")
+        self.ui.predictionTableWidget.setSortingEnabled(False)
+        item = self.ui.predictionTableWidget.verticalHeaderItem(0)
+        item.setText("Dissolved Oxygen (mg/l)")
+        item = self.ui.predictionTableWidget.verticalHeaderItem(1)
+        item.setText("pH")
+        item = self.ui.predictionTableWidget.verticalHeaderItem(2)
+        item.setText("Conductivity (µmhos/cm)")
+        item = self.ui.predictionTableWidget.verticalHeaderItem(3)
+        item.setText("B.O.D. (mg/l)")
+        item = self.ui.predictionTableWidget.verticalHeaderItem(4)
+        item.setText("Nitrate (mg/l)")
+        item = self.ui.predictionTableWidget.verticalHeaderItem(5)
+        item.setText("Fecal Coliform (MPN/100ml)")
+        item = self.ui.predictionTableWidget.verticalHeaderItem(6)
+        item.setText("Total Coliform (MPN/100ml)")
+        item = self.ui.predictionTableWidget.horizontalHeaderItem(0)
+        item.setText("Value")
+        self.ui.predictionTableWidget.setItemDelegateForColumn(0, self.delegate)
+        # self.ui.predictPushButton.setEnabled(False)
+        self.ui.predictPushButton.clicked.connect(self.prediction)
+        # self.ui.autofillPushButton.setEnabled(False)
+        self.ui.autofillPushButton.clicked.connect(self.auto_fill)
+        self.addDockWidget(QtCore.Qt.DockWidgetArea(1), self.ui.predictionDockWidget)
+
+    def result_docker(self):
+        self.ui.resultDockWidget = QtWidgets.QDockWidget(self)
+        self.ui.resultDockWidget.setMinimumSize(QtCore.QSize(316, 170))
+        self.ui.resultDockWidget.setObjectName("resultDockWidget")
+        self.ui.resultDockWidgetContents = QtWidgets.QWidget()
+        self.ui.resultDockWidgetContents.setObjectName("resultDockWidgetContents")
+        self.ui.resultSplitter = QtWidgets.QSplitter(self.ui.resultDockWidgetContents)
+        self.ui.resultSplitter.setGeometry(QtCore.QRect(10, 10, 291, 141))
+        self.ui.resultSplitter.setOrientation(QtCore.Qt.Orientation.Vertical)
+        self.ui.resultSplitter.setObjectName("resultSplitter")
+        self.ui.resultLabel = QtWidgets.QLabel(self.ui.resultSplitter)
+        self.ui.resultLabel.setObjectName("resultLabel")
+        self.ui.resultOutput = QtWidgets.QTextEdit(self.ui.resultSplitter)
+        self.ui.resultOutput.setObjectName("resultOutput")
+        self.ui.resultDockWidget.setWidget(self.ui.resultDockWidgetContents)
+        self.ui.resultLabel.setText("Result")
+        self.ui.resultOutput.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                     "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                     "p, li { white-space: pre-wrap; }\n"
+                                     "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                     "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>")
+        self.ui.resultOutput.setReadOnly(True)
+        self.addDockWidget(QtCore.Qt.DockWidgetArea(1), self.ui.resultDockWidget)
+
+    def build_about(self):
+        dialog = QDialog(self)
+        dialog.ui = AboutDialog()
+        dialog.ui.setupUi(dialog)
+        dialog.exec()
+        dialog.show()
 
 
 class MyToolBar(NavigationToolbar):
